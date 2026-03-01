92d1e546accbdbb40600f8313f9a0fccede84c6a
AncientMystic <62780271+AncientMystic@users.noreply.github.com>
2026-03-01 23:38:37 +0000

    Update scheduled task to use new script path

--- Windows-vGPU-sceduled-task.ps1
+++ Windows-vGPU-sceduled-task.ps1
@@ -1,6 +1,7 @@
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -File C:\Scripts\Update-vGPUToken.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -File C:\Scripts\Windows-Update-vGPUToken.ps1"
 $trigger = New-ScheduledTaskTrigger -Daily -DaysInterval 30 -At 03:00
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
 
 Register-ScheduledTask -TaskName "Update vGPU Token" -Action $action -Trigger $trigger -Settings $settings -Principal $principal -Description "Refresh NVIDIA vGPU license token"
